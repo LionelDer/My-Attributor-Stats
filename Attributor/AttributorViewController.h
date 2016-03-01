f0cbf8d0a9166c9f633d96b367787c2c060949e4
LionelDer <lionelder877@gmail.com>
2016-03-01 20:15:21 +0800

    My_Attributor_Stats
    在UITextView的文字可以根據使用者喜好選取textStorage的範圍，而這個範圍可以改變他的UIColor,並使用preferredF
    ontsSizeChanged根據使用者喜好的字體大小進行切換
    利用UINavigationController在不同的view做切換，在另外一個view上做統計.

---
+++ Attributor/AttributorViewController.h
@@ -0,0 +1,13 @@
+//
+//  AttributorViewController.h
+//  Attributor
+//
+//  Created by user on 2015/11/9.
+//  Copyright (c) 2015年 fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributorViewController : UIViewController
+
+@end
---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by user on 2015/11/9.
+//  Copyright (c) 2015年 fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
